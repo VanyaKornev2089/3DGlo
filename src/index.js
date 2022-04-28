8f2c0bb4266515e214b66719a578437c56ffd2b3
VanyaKornev2089 <94173875+VanyaKornev2089@users.noreply.github.com>
2022-04-28 13:51:04 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
-import one from './modules/one'
-import two from './modules/two'
+import timer from './modules/timer'
+import menu from './modules/menu'
+import modal from './modules/modal'
+import calculator from './modules/calculator'
+import form from './modules/form'
+import tabs from './modules/tabs'
+import slider from './modules/slider'
+import calc from './modules/calc'
 
 
-one()
-two()
+timer('30 april 2022')
+menu()
+modal()
+calculator()
+form()
+tabs()
+slider()
+calc(100)
